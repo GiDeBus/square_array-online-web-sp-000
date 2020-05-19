fb9697dad2a55ae034a82932909ee38b38a92ed4
Ignacio Del Busto <gidelbusto@gmail.com>
2020-05-19 03:48:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,12 @@ def square_array(array)
     counter += 1
   end
 end
+
+def square_array(array)
+  new_array = []
+  array.each do |element|
+    element = element * element
+    new_array.add(element)
+  end
+  return new_array
+end
